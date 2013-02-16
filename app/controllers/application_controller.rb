654ad78d7202a06a41b72d80887429a2e786060e
RichardWigley <richard.wigley@gmail.com>
2013-02-16 08:03:27 +0000

    Cosmetic change. Allowing Linux users access to the website

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,8 @@ class ApplicationController < ActionController::Base
   Browser = Struct.new(:platform, :browser, :version)
   #  not this is the minium browser we accept
   SupportedBrowser = [ Browser.new("iPhone", "Safari", "5.0") ,
-                       Browser.new("iPod","Safari", "4.0")]
-                       # Browser.new("X11","Chrome", "24")]
+                       Browser.new("iPod","Safari", "4.0"),
+                       Browser.new("X11","Chrome", "24")]
 
   def allowed_device?
     user_agent = UserAgent.parse(request.user_agent)
