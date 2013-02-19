8a662c8dc9e1f4e50d12b845b7d494e6d75ec0ed
RichardWigley <richard.wigley@gmail.com>
2013-02-19 17:36:59 +0000

    Removing platform selection for testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,9 +14,10 @@ class ApplicationController < ActionController::Base
                        Browser.new("iPod","Safari", "4.0")]
 
   def allowed_device?
-    user_agent = UserAgent.parse(request.user_agent)
-    logger.info "UserAgent Platform " + user_agent.platform
-    SupportedBrowser.detect {|browser| user_agent.platform == browser.platform}
+    # user_agent = UserAgent.parse(request.user_agent)
+    # logger.info "UserAgent Platform " + user_agent.platform
+    # SupportedBrowser.detect {|browser| user_agent.platform == browser.platform}
+    true
   end
 
 end
